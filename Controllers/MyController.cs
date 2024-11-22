6910940a52ab7ecfc261f13792357ef3688296e3
Gaurav Agarwal <gaagarwal@microsoft.com>
2024-11-22 15:50:13 +0530

    initial commit

---
+++ Controllers/MyController.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Dapr.Client;
+using System.Text.Json.Serialization;
+
+namespace ADFDaprDemo.Controllers
+{
+    public class RequestData
+    {
+        [JsonPropertyName("data")]
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonPropertyName("value")]
+        public Value Value { get; set; }
+    }
+
+    public class Value
+    {
+        [JsonPropertyName("orderId")]
+        public string OrderId { get; set; }
+    }
+
+    [ApiController]
+    [Route("api/calladf")]
+    public class MyController : ControllerBase
+    {
+        private readonly DaprClient _daprClient;
+
+        public MyController(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] object request)
+        {
+            // create a new request object
+            var newRequest = new RequestData
+            {
+                Data = new Data
+                {
+                    Value = new Value
+                    {
+                        OrderId = "Order22"
+                    }
+                }
+            };
+
+            try
+            {
+                var response = await _daprClient.InvokeMethodAsync<object, object>(
+                    appId: "adfdaprdemo-funcapp",
+                    methodName: "CreateNewOrder",
+                    data: newRequest
+                );
+
+                return Ok(new { Message = "Post request received" });
+            }
+            catch (Exception ex) {
+                return StatusCode(500, new { Error = "An error occurred while invoking Dapr service", Details = ex.Message });
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+namespace adfdemo_frontend_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddDaprClient();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
